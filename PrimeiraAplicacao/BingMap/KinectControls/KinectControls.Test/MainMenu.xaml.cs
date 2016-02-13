55dca36080d15ba373227089d34c270f82335718
Renato Souza <renatosouzav@hotmail.com>
2016-02-13 11:32:44 -0300

    Tela menu principal (e imagem) chamando a tela do mapa

---
+++ PrimeiraAplicacao/BingMap/KinectControls/KinectControls.Test/MainMenu.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KinectControls.Test
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow map = new MainWindow();
+            map.Show();
+            this.Close();
+        }
+    }
+}
