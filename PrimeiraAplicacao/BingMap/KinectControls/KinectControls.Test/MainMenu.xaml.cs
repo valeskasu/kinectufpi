58d4ea476fd7681d354f95b2c5726053f93c6fcd
valeska_uchoa <valeskauchoa@gmail.com>
2016-02-15 14:25:00 -0300

    alterações no menu

--- PrimeiraAplicacao/BingMap/KinectControls/KinectControls.Test/MainMenu.xaml.cs
+++ PrimeiraAplicacao/BingMap/KinectControls/KinectControls.Test/MainMenu.xaml.cs
@@ -19,6 +19,8 @@ namespace KinectControls.Test
     /// </summary>
     public partial class MainMenu : Window
     {
+       
+
         public MainMenu()
         {
             InitializeComponent();
